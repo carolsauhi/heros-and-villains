e784c9966bd8c72858c4f4b86c6ba67c097bc3eb
carolsauhi <carol_sauhi@hotmail.com>
2025-06-02 20:20:36 -0300

    Creating a button to clear the input. Adjusted the button to close the modal.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import './Header.css';
-import logo from '../../assets/Cat_Gloss.png';
 
 const Header = ({ searchTerm, setSearchTerm }) => {
+
   return (
     <header className="header">
-      <img src={logo} alt="Cat Gloss Logo" className="logo" />
+      <h1 className="app-title">
+        <span className="heroes">Heroes</span>
+        <span className="and-symbol"> &amp; </span>
+        <span className="villains">Villains</span>
+      </h1>
+
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
     </header>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import './SearchBar.css';
+import clear from '../../assets/clear.svg'
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
+  const handleClear = () => setSearchTerm('');
+
   return (
-    <input
-      className="search-input"
-      type="text"
-      placeholder="Type here..."
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
-    />
+    <div className="search-wrapper">
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Type here..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <button onClick={handleClear} className="clear-button">
+        <img src={clear} alt="clear button" />
+      </button>
+    </div>
+
   );
 };
 
